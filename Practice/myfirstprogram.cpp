36cd8f70395c877c6cf83b58890de2e2d1bddc18
John <johnisaacarthur@gmail.com>
2025-10-05 19:24:26 -0600

    AddingOOP

--- Practice/myfirstprogram.cpp
+++ Practice/myfirstprogram.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
+    struct player {
+        int pnum;
+        string name;
+        int lives;
+        bool plive;
+    };
+
+    player mainplayer;
+    mainplayer.pnum = 1;
+    mainplayer.name = "mario";
+    mainplayer.lives = 1;
+    mainplayer.plive = true;
+
     cout << "Type your name here";
 
     string wordsnow = "New concept";
