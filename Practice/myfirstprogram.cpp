d8833ea88665d842381da96a9600039cb972a285
John <johnisaacarthur@gmail.com>
2025-10-13 15:58:11 -0600

    Added a return function to understand

--- Practice/myfirstprogram.cpp
+++ Practice/myfirstprogram.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+float toCelcius(float f) {
+    return (5.0/9.0)*(f-32.0);
+}
+
 int main()
 {
     struct player {
@@ -13,13 +17,15 @@ int main()
         int lives;
         bool plive;
     };
-
+    float farv = 68.0;
+    float celcv = toCelcius(farv);
+    cout << farv << " in celcius is " << celcv;
     player mainplayer;
     mainplayer.pnum = 1;
     mainplayer.name = "mario";
     mainplayer.lives = 1;
     mainplayer.plive = true;
-
+    cout << endl;
     cout << "Type your name here";
 
     string wordsnow = "New concept";
