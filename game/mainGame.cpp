bf3dd284a0bffdd37a30f0e2c8e124868b9b6ffb
John <johnisaacarthur@gmail.com>
2025-12-01 15:59:18 -0700

    PlayerObj

--- game/mainGame.cpp
+++ game/mainGame.cpp
@@ -1,7 +1,16 @@
+#include <string>
+
 using namespace std;
+
+
+int player(string pName, int initX, int initY) {
+    int alv = 1;
+    
+    return alv;
+}
 
 int mainGame() {
     
-    
+
     return 0;
 }
